33b8590c5e2a038bc862d07b47871857320f3f91
Leonel <leosmac@Leonels-MacBook-Pro.local>
2019-05-21 15:17:54 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,8 @@
+
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("Users flipped" + cardOne);
+console.log("Users flipped" + cardThree);
